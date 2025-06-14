2372ccf9c4f37ae033a520c06888d58224dc7c29
AbdulSamad94 <abdulsamadsiddiqui2000@gmail.com>
2025-06-14 11:53:12 +0500

    cors-fix

--- api/email.js
+++ api/email.js
@@ -1,6 +1,18 @@
 import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+
+    if (req.method === "OPTIONS") {
+        res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+        res.setHeader(
+            "Access-Control-Allow-Headers",
+            "X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization"
+        );
+        return res.status(200).end();
+    }
+
     if (req.method !== "POST") {
         return res.status(405).json({ message: "Method not allowed" });
     }
